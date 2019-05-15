38477aaf5f76b4e215d058d939269a099f13f600
malaretv <malaretv@gmail.com>
2019-05-15 09:33:42 -0400

    Upgrade ES and change sylvester dep
    Should work as a modern ES package now.
    
    Removed sylvester-node dependency that was causing problems

--- test/orbit.js
+++ test/orbit.js
@@ -1,22 +1,22 @@
-require("sylvester");
+import chai from 'chai';
+import { Vector } from '@kaosat-dev/sylvester/src/index';
+import Kepler from '../lib/kepler';
 
-var Kepler = require("../lib/kepler"),
-    expect = require("chai").expect;
-
+const { expect } = chai;
 describe("Orbit", function() {
   describe("Calculating orbital elements from state vectors", function() {
     var orbit;
 
     before(function() {
-      var r = $V([-6045, -3490, 2500]),
-          v = $V([-3.457, 6.618, 2.533]);
+      var r = Vector.create([-6045, -3490, 2500]),
+          v = Vector.create([-3.457, 6.618, 2.533]);
 
       orbit = new Kepler.Orbit(r, v);
     });
 
     it("angularMomentum", function() {
       var actual = orbit.angularMomentum(),
-          expected = $V([-25385.17, 6669.485, -52070.74]);
+          expected = Vector.create([-25385.17, 6669.485, -52070.74]);
       expect(actual.eql(expected)).to.be.true;
     });
 
@@ -28,7 +28,7 @@ describe("Orbit", function() {
 
     it("eccentricity", function() {
       var actual = orbit.eccentricity(),
-          expected = $V([-0.091604, -0.142207, 0.026443]);
+          expected = Vector.create([-0.091604, -0.142207, 0.026443]);
       expect(actual.eql(expected)).to.be.true;
     });
 
@@ -58,7 +58,7 @@ describe("Orbit", function() {
 
     it("nodeLine", function() {
       var actual = orbit.nodeLine(),
-          expected = $V([-6669.485, -25385.17, 0.0]);
+          expected = Vector.create([-6669.485, -25385.17, 0.0]);
       expect(actual.eql(expected)).to.be.true;
     });
 
@@ -103,8 +103,8 @@ describe("Orbit", function() {
     var orbit;
 
     beforeEach(function() {
-      var r = $V([7000, -12124, 0]),
-          v = $V([2.6679, 4.6210, 0]);
+      var r = Vector.create([7000, -12124, 0]),
+          v = Vector.create([2.6679, 4.6210, 0]);
 
       orbit = new Kepler.Orbit(r, v);
     });
@@ -118,8 +118,8 @@ describe("Orbit", function() {
     it("update", function() {
       orbit = orbit.update(3600);
 
-      var rExpected = $V([-3297.768625, 7413.396645, 0.0]),
-          vExpected = $V([-8.297603, -0.9640449, -0.0]);
+      var rExpected = Vector.create([-3297.768625, 7413.396645, 0.0]),
+          vExpected = Vector.create([-8.297603, -0.9640449, -0.0]);
 
       expect(orbit.r.eql(rExpected)).to.be.true;
       expect(orbit.v.eql(vExpected)).to.be.true;
@@ -142,8 +142,8 @@ describe("Orbit", function() {
     });
 
     it("state", function() {
-      var rExpected = $V([-4039.895923, 4814.560480, 3628.624702]),
-          vExpected = $V([-10.385987, -4.771921, 1.743875]);
+      var rExpected = Vector.create([-4039.895923, 4814.560480, 3628.624702]),
+          vExpected = Vector.create([-10.385987, -4.771921, 1.743875]);
 
       expect(orbit.r.eql(rExpected)).to.be.true;
       expect(orbit.v.eql(vExpected)).to.be.true;
@@ -165,8 +165,8 @@ describe("Orbit", function() {
     });
 
     it("state", function() {
-      var rExpected = $V([1311.563646, 4699.598648, 4701.881415]),
-          vExpected = $V([-5.641883, 4.379639, -2.8037407]);
+      var rExpected = Vector.create([1311.563646, 4699.598648, 4701.881415]),
+          vExpected = Vector.create([-5.641883, 4.379639, -2.8037407]);
 
       expect(orbit.r.eql(rExpected)).to.be.true;
       expect(orbit.v.eql(vExpected)).to.be.true;
